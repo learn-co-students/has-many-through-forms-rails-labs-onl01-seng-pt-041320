341b600ed25e08c9ad027a25a308246dec239845
asif-kamal <asif.kamal1489@gmail.com>
2020-09-02 23:59:45 -0400

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,17 @@
+require 'pry'
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
+  accepts_nested_attributes_for :user, reject_if: proc{|attributes|attributes["username"].blank?}
+
+  def user_attributes=(user_attributes)
+
+      if user_attributes[:username].present?
+      #binding.pry
+      user = User.find_or_create_by(username: user_attributes[:username])
+      self.user = user
+
+    end
+  end
 
 end
