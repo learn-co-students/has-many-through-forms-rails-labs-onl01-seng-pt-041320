e36d0eb0212d7d53cf8429cabceffb0dd528a32a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-09-12 11:18:36 -0700

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
 
+  def new
+    User.new
+  end
+
+  def create
+    User.create(user_params)
+  end
+
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username)
   end
 
 end
