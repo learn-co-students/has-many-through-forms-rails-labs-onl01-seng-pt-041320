35d4c9687a3987d8c7c834fd2b53ba0c2a0982e2
Romario Gustave <romagustave10@gmail.com>
2020-09-19 02:27:20 +0000

    Done.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
+  validates_uniqueness_of :name
   has_many :post_categories
   has_many :posts, through: :post_categories
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,18 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_hashes)
+    # {"0"=>{"name"=>"new category 1"}, "1"=>{"name"=>"new category 2"}}
 
+    categories_hashes.values.each do |category_attributes|
+      # create a new category if this post doesn't already have this category
+      #  find or create the category regardless of whether this post has it
+      category = Category.find_or_create_by(category_attributes) unless category_attributes[:name] == ""
+      # need to create a category that is already associated with this post
+      # and need to make sure that this category already doesn't exist by name
+      if category
+          self.categories << category
+      end
+    end
+  end
 end
